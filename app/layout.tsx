36a1d8ba1e86690018e3be74c7400124a9cd9663
teddymillion <>
2026-03-15 19:28:41 +0300

    changed the favicon to my logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,8 @@ export const metadata: Metadata = {
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/favicon.ico',
+        type: 'image/x-icon',
       },
     ],
     apple: '/apple-icon.png',
